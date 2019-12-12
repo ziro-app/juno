3b4aad8fd1608835b8cb745883fc14f34be6e61d
AhmadForhat <ahmad.forhat@gmail.com>
2019-12-12 12:26:45 -0300

    teste sem ou na porta

--- src/index.js
+++ src/index.js
@@ -115,7 +115,7 @@ app.post('/transferencia-saldo', async (req,res) => {
     }
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log('Sucess');
